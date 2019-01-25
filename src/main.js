8793f0afc62a0730610c0a02477de3d6f27d19b9
lixue0919 <442019696@qq.com>
2019-01-25 11:41:57 +0800

    修改路由文件位置，单独创建文件夹  by li.xue 2019/1/25

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './common/router'
+import router from './router'
 import axios from 'axios';
 import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
