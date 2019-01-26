bda428a39c4e5244be90e3f4aa2211d6184181ef
lixue0919 <442019696@qq.com>
2019-01-27 00:17:55 +0800

    路由与菜单的绑定  by li.xue 2019/1/27

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,163 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-// 路由数据
-import routes from './routes'
-import axios from 'axios'
-Vue.use(Router)
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
-const router = new Router({
-  routes
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: resolve => require(['@/views/common/Login'], resolve)
+    },
+    {
+      path: '/index',
+      name: 'index',
+      meta: {auth: true},
+      component: resolve => require(['@/views/common/Index'], resolve),
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          meta: {auth: true, title: "首页"},
+          component: resolve => require(['@/views/home/Home'], resolve)
+        },
+        {
+          path: '/prediction/exhprediction',
+          name: 'exhprediction',
+          meta: {auth: true, title: "展会报名管理"},
+          component: resolve => require(['@/views/prediction/Exhprediction'], resolve)
+        },
+        {
+          path: '/prediction/exhprediction_approve',
+          name: 'exhpredictionApprove',
+          meta: {auth: true, title: "展会报名审核"},
+          component: resolve => require(['@/views/prediction/ExhpredictionApprove'], resolve)
+        },
+        {
+          path: '/prediction/exhibitor',
+          name: 'exhibitor',
+          meta: {auth: true, title: "展商用户管理"},
+          component: resolve => require(['@/views/prediction/Exhibitor'], resolve)
+        },
+        {
+          path: '/venue',
+          name: 'venue',
+          meta: {auth: true, title: "展馆管理"},
+          component: resolve => require(['@/views/layout/MainDom'], resolve),
+          children: [
+            {
+              path: '/plan/plan',
+              name: 'plan',
+              meta: {auth: true, title: "展馆平面图"},
+              component: resolve => require(['@/views/venue/Plan'], resolve)
+            },
+            {
+              path: '/venue/trader',
+              name: 'trader',
+              meta: {auth: true, title: "展馆管理"},
+              component: resolve => require(['@/views/venue/Trader'], resolve)
+            },
+          ]
+        },
+        {
+          path: '/booth',
+          name: 'booth',
+          meta: {auth: true, title: "展位管理"},
+          component: resolve => require(['@/views/layout/MainDom'], resolve),
+          children: [
+            {
+              path: '/booth/position',
+              name: 'position',
+              meta: {auth: true, title: "展位管理"},
+              component: resolve => require(['@/views/booth/Position'], resolve)
+            },
+            {
+              path: '/booth/position_design',
+              name: 'positionDesign',
+              meta: {auth: true, title: "展位设计"},
+              component: resolve => require(['@/views/booth/PositionDesign'], resolve)
+            },
+            {
+              path: '/booth/positiontype',
+              name: 'positionType',
+              meta: {auth: true, title: "展位分类"},
+              component: resolve => require(['@/views/booth/PositionType'], resolve)
+            },
+          ]
+        },
+        {
+          path: '/statistical',
+          name: 'statistical',
+          meta: {auth: true, title: "统计分析"},
+          component: resolve => require(['@/views/layout/MainDom'], resolve),
+          children: [
+            {
+              path: '/statistical/statistical_product',
+              name: 'statisticalProduct',
+              meta: {auth: true, title: "按产品类型统计"},
+              component: resolve => require(['@/views/statistical/StatisticalProduct'], resolve)
+            },
+            {
+              path: '/statistical/statistical_area',
+              name: 'statistical_area',
+              meta: {auth: true, title: "按光地展位面积统计"},
+              component: resolve => require(['@/views/statistical/StatisticalArea'], resolve)
+            },
+            {
+              path: '/statistical/statistical_confirm',
+              name: 'statisticalConfirm',
+              meta: {auth: true, title: "按确认信息统计"},
+              component: resolve => require(['@/views/statistical/StatisticalConfirm'], resolve)
+            },
+          ]
+        },
+        {
+          path: '/system',
+          name: 'system',
+          meta: {auth: true, title: "系统管理"},
+          component: resolve => require(['@/views/layout/MainDom'], resolve),
+          children: [
+            {
+              path: '/system/user',
+              name: 'user',
+              meta: {auth: true, title: "用户管理"},
+              component: resolve => require(['@/views/system/User'], resolve)
+            },
+            {
+              path: '/system/role',
+              name: 'role',
+              meta: {auth: true, title: "角色管理"},
+              component: resolve => require(['@/views/system/Role'], resolve)
+            },
+            {
+              path: '/system/resource',
+              name: 'resource',
+              meta: {auth: true, title: "资源管理"},
+              component: resolve => require(['@/views/system/Resource'], resolve)
+            },
+            {
+              path: '/system/permission',
+              name: 'permission',
+              meta: {auth: true, title: "权限管理"},
+              component: resolve => require(['@/views/system/Permission'], resolve)
+            },
+            {
+              path: '/system/codelist',
+              name: 'codelist',
+              meta: {auth: true, title: "代码集管理"},
+              component: resolve => require(['@/views/system/Codelist'], resolve)
+            },
+            {
+              path: '/system/mail',
+              name: 'mail',
+              meta: {auth: true, title: "邮箱管理"},
+              component: resolve => require(['@/views/system/Mail'], resolve)
+            },
+          ]
+        },
+      ]
+    }
+  ]
 })
+
 export default router
--- src/views/layout/AsideDom.vue
+++ src/views/layout/AsideDom.vue
@@ -8,23 +8,23 @@
   -->
     <el-menu :default-active="$route.path" :collapse="isCollapse" mode="vertical" :unique-opened="isUniqueOpened" :router="isRouter"
               background-color="#463132" text-color="#ffffff" active-text-color="#E40613">
-      <template v-for="route in routes">
-        <template v-if="route.children">
-          <el-submenu :index="route.path">
+      <template v-for="menu in menus">
+        <template v-if="menu.children">
+          <el-submenu :index="menu.resourcename">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>{{route.meta.title}}</span>
+              <span>{{menu.resourceinfo}}</span>
             </template>
-            <el-menu-item v-for="(children, childrenIndex) in route.children" :key="childrenIndex" :index="children.path">
-            {{children.meta.title}}
+            <el-menu-item v-for="(child, childIndex) in menu.children" :key="childIndex" :index="child.resourcename">
+              {{child.resourceinfo}}
               <!--<router-link :to="children.path">{{children.meta.title}}</router-link>-->
             </el-menu-item>
           </el-submenu>
         </template>
         <template v-else>
-          <el-menu-item :index="route.path">
+          <el-menu-item :index="menu.resourcename">
             <i class="el-icon-menu"></i>
-            <span>{{route.meta.title}}</span>
+            <span>{{menu.resourceinfo}}</span>
           </el-menu-item>
         </template>
       </template>
@@ -33,61 +33,36 @@
 </template>
 
 <script>
+
 export default {
   name: 'AsideDom',
-  data: function(){
+  data(){
     return {
       isCollapse: false,
       isUniqueOpened: true,
       isRouter: true,
+      menus:[],
     };
   },
-  computed: {
-    routes() {
-      /**
-      if(this.$store.state.menus.length === 0) {
-        var menus = [];
-        if(this.$store.state.user.resourceTrees == undefined) {
-          var resourceTrees = JSON.parse(this.$store.state.user).resourceTrees;
-        } else {
-          var resourceTrees = this.$store.state.user.resourceTrees;
-        }
-        for (var i in resourceTrees) {
-          var children = [];
-          for (var j in resourceTrees[i].children) {
-            var tempChild = {
-              path: resourceTrees[i].children[j].resourcename,
-              name: resourceTrees[i].children[j].resourcename,
-              meta: {title: resourceTrees[i].children[j].resourceinfo, auth: true},
-              component(resolve) {
-                let componentPathChild = '@/views' + resourceTrees[i].children[j].resourcename;
-                require([`${componentPathChild}.vue`], resolve);
-              },
-            };
-            children.push(tempChild);
-          }
-          var temp = {
-            path: resourceTrees[i].resourcename,
-            name: resourceTrees[i].resourcename,
-            meta: {title: resourceTrees[i].resourceinfo, auth: true},
-            component(resolve) {
-              let componentPath = '@/views/layout/Index';
-              require([`${componentPath}.vue`], resolve);
-            },
-            children: children.length>0 ? children : null
-          };
-          menus.push(temp);
-          this.$router.options.routes[1].children.push(temp);
-        }
-        this.$store.commit('$_setMenus', menus);
+  methods: {
+    //初始化菜单
+    initMenu(){
+      if (localStorage.getItem("shiroUser")) {
+        this.menus = JSON.parse(localStorage.getItem("shiroUser")).resourceTrees;
       }
-       */
-      console.log(2222222222222);
-      console.log(this.$router.options.routes);
-      console.log(this.$router.options.routes[1].children);
-      
-      return this.$router.options.routes[1].children;
-    },
+      /**通过请求方式获取菜单，暂不好用，跨域问题  by li.xue */
+      /**
+      let vm = this;
+      vm.$axios.get('/getMenu').then(function (res) {
+        console.log(res.data);
+      }.bind(this), function (error) {
+        console.log(error)
+      })
+      */
+    }
+  },
+  mounted() {
+      this.initMenu()
   },
 }
 </script>
