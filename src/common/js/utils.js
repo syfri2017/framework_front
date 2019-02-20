833d3cd291161f9ac4a3dee018d560e3d8024e94
yushch <1096853079@qq.com>
2019-02-20 14:41:18 +0800

    暂时提交用户管理，分页

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -13,26 +13,7 @@ export default {
                 return true;
             }
         };
-        //当前页修改事件
-        Vue.prototype.currentPageChange = function(val) {
-            debugger;
-            if(this.currentPage != val){
-                this.currentPage = val;
-                this.searchClick('page');
-            }
-        };
-        //分页大小修改事件
-        Vue.prototype.pageSizeChange = function(val) {
-            this.pageSize = val;
-            if(val == 10){
-                tableheight = tableheight10;
-            }else if(val == 20){
-                tableheight = tableheight20;
-            }else if(val == 30){
-                tableheight = tableheight30;
-            }
-            this.searchClick('page');
-        };
+       
 
 
     
---
+++ src/components/paginator/index.vue
@@ -0,0 +1,45 @@
+<template>
+<!--列表底部工具条和分页符-->
+<el-row type="flex" justify="end">
+	<el-col :span="18">
+		<el-pagination @size-change="pageSizeChange"
+			@current-change="currentPageChange" 
+			:current-page="this.$parent.currentPage"
+			:page-sizes="[10, 20, 30, 50]" 
+			:page-size="this.$parent.pageSize" 
+			layout="total, prev, pager, next, jumper" 
+			:total=parseInt(this.$parent.total)>
+		</el-pagination>
+	</el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+	methods: {
+		//当前页修改事件
+        currentPageChange : function(val) {
+            if(this.$parent.currentPage != val){
+                this.$parent.currentPage = val;
+                this.$parent.searchClick('page');
+            }
+        },
+        //分页大小修改事件
+        pageSizeChange : function(val) {
+            this.$parent.pageSize = val;
+            if(val == 10){
+                tableheight = tableheight10;
+            }else if(val == 20){
+                tableheight = tableheight20;
+            }else if(val == 30){
+                tableheight = tableheight30;
+            }
+            this.$parent.searchClick('page');
+        },
+	}
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
