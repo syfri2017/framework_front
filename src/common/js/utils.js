3693bb11918d4e5db9655d36f3c5b9817d599704
lixue0919 <442019696@qq.com>
2019-02-26 08:30:29 +0800

    table中日期格式化  by li.xue 2019/2/26

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -1,21 +1,24 @@
 export default {
-    install(Vue, options) {
-        Vue.prototype.testAAA = function (msg) {
-            return msg;
-        };
-        //验证当前用户是否有操作权限
-        Vue.prototype.hasPermission = function(val){
-            var permissions = JSON.parse(localStorage.getItem("CURRENTUSER")).permissions;
-            var index = permissions.indexOf(val);
-            if(index == -1){
-                return false;
-            }else{
-                return true;
-            }
-        };
-       
-
-
-    
-    }
+  install(Vue, options) {
+      
+      //验证当前用户是否有操作权限
+      Vue.prototype.hasPermission = function(val){
+          var permissions = JSON.parse(localStorage.getItem("CURRENTUSER")).permissions;
+          var index = permissions.indexOf(val);
+          if(index == -1){
+              return false;
+          }else{
+              return true;
+          }
+      };
+      //table日期格式化
+      Vue.prototype.tableDateFormat = function (row, column) { 
+        var rowDate = row[column.property];
+        if (rowDate == null || rowDate == "") {
+            return '';
+        } else {
+            return dateFormat(rowDate);
+        }
+      }
+  }
 }
