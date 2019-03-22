01a5a72f69a2e72fec39de9e18d1d523e2cd94d4
yushch <1096853079@qq.com>
2019-03-22 10:01:56 +0800

    中文登录梳理

--- src/views/exhibition/login/ch/Login.vue
+++ src/views/exhibition/login/ch/Login.vue
@@ -6,25 +6,28 @@
         <div class="login-form" >
           <div class="filed left">
             <router-link :to="{path:'/exhibition/login/ch/Administrator'}"><i class="iconfont icon-yonghu icou"></i></router-link>
-            <span >用户登录</span>
+            <span class="formTitleStyle">用户登录</span>
             <span class="signstyle">没有账户?<router-link :to="{path:'/exhibition/login/ch/Register'}"><a>去注册</a></router-link></span>
           </div>
           <el-form ref="GLYloginForm" id="GLYloginForm" autocomplete="off" name="loginform"  method="post">
             <div class="filed">
-              <el-input placeholder="用户名" v-model="GLYusername" prefix-icon="iconfont icon-username" @blur="mobileCheck"></el-input>
+              <el-input placeholder="用户名" v-model="username" prefix-icon="iconfont icon-username" @blur="mobileCheck"></el-input>
+              <p class="alert" v-show="usernameAlertFlag">*请输入正确手机号</p>
             </div>
             <div class="filed">
-              <el-input placeholder="密码" v-model="GLYpassword" prefix-icon="iconfont icon-password" type="password"></el-input>
+              <el-input placeholder="密码" v-model="password" prefix-icon="iconfont icon-password" type="password" @blur="passwordCheck"></el-input>
+              <p class="alert1" v-show="passwordAlertFlag">*请输入密码</p>
             </div>
             <div class="filed">
-              <el-input placeholder="验证码" class="yanzhengma_input"  v-model="picLyanzhengma" prefix-icon="iconfont icon-validate"></el-input>
+              <el-input placeholder="验证码" class="yanzhengma_input"  v-model="picLyanzhengma" prefix-icon="iconfont icon-validate" @blur="validateCheck"></el-input>
               <input type="button"  class="verification1 bk" id="code" @click="createCode"  v-model="checkCode"/>
+              <p class="alert2" v-show="validateAlertFlag">*验证码错误</p>
             </div>
             <div class="filed right">
               <span class="muchtab"><router-link :to="{path:'/exhibition/login/ch/ForgetUsername'}"><a>忘记用户名</a></router-link>  |  <router-link :to="{path:'/exhibition/login/ch/ForgetPassword'}"><a>忘记密码</a></router-link>  |  <router-link :to="{path:'/exhibition/login/ch/Reset'}"><a>重置账户</a></router-link></span>
             </div>
             <div class="filed lgin">
-              <el-button type="danger" @click="GLYlogin" round>登录</el-button>
+              <el-button type="danger" @click="login" round>登录</el-button>
             </div>
           </el-form>
         </div>
@@ -39,87 +42,106 @@ export default {
   name: 'Login',
   data () {
     return {
-        GLYusername: "",
-        GLYpassword: "",
-        GLYsrc: "/imageCode",
-        GLYvalidateCode: "",
-        GLYmessages: "",
-        GLYloginType: "MyShiro",
-        userPhone:'',
-        dialog: false,
-        UserPhone:'',
-        LUserPsd:'',
+        username: "",
+        password: "",
+        //GLYloginType: "MyShiro",
         picLyanzhengma:'',
         checkCode:'',
         // 校验标识符
-        // 注册校验标识
-        mobileAlertFlag: false,
-        // messageCodeAlertFlag: false,
-        // password1TipFlag: false,
-        // password1AlertFlag: false,
-        // password2AlertFlag: false
-
+        usernameAlertFlag: false,
+        passwordAlertFlag: false,
+        validateAlertFlag: false
     }
   },
   methods:{
-      
-      // 输入框校验
-      mobileCheck() {
-        if (!/^1[34578]\d{9}$/.test(this.mobile)) {
-          this.mobileAlertFlag = true;
-          return false;
-        } else {
-          this.mobileAlertFlag = false;
-          return true;
-        }
-      },
-     // 图片验证码
-      createCode(){
-          code = "";    
-          var codeLength = 4;//验证码的长度   
-          var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',   
+    // 验证用户名是否为手机号
+    mobileCheck() {
+      if (!/^1[34578]\d{9}$/.test(this.username)) {
+        //用户名不正确，显示红字提醒
+        this.usernameAlertFlag = true;
+        return false;
+      } else {
+        this.usernameAlertFlag = false;
+        return true;
+      }
+    },
+    //密码校验
+    passwordCheck() {
+      //密码为空
+      if (this.password == '' || this.password == null) {
+        this.passwordAlertFlag = true;
+        return false;
+      } else {
+        this.passwordAlertFlag = false;
+        return true;
+      }
+    },
+    //验证码校验
+    validateCheck() {
+      //验证码错误
+      if (this.picLyanzhengma.toUpperCase() != this.checkCode) {
+        this.validateAlertFlag = true;
+        //刷新Verification Code 
+        this.createCode();  
+        this.picLyanzhengma = '';
+        return false;
+      }else {
+        //清空所有红字提醒
+        this.validateAlertFlag = false;
+        return true;
+      }
+    },
+    // 图片验证码
+    createCode(){
+      code = "";    
+      var codeLength = 4;//验证码的长度   
+      var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',   
            'S','T','U','V','W','X','Y','Z');//随机数   
-          for(var i = 0; i < codeLength; i++) {//循环操作   
-              var index = Math.floor(Math.random()*36);//取得随机数的索引（0~35）   
-              code += random[index];//根据索引取得随机数加到code上   
-          }   
-              this.checkCode = code;//把code值赋给验证码   
-      },
-      // 失焦验证图和密码
-      checkLpicma(){
-          this.picLyanzhengma.toUpperCase();//取得输入的验证码并转化为大写         
-          if(this.picLyanzhengma == '') {
-              // $(".login_content1 span:eq(2)").text("请输入验证码")
-              // $(".login_content1 span:eq(2)").removeClass("disappear");
-          }else if(this.picLyanzhengma.toUpperCase() != this.checkCode ) { //若输入的验证码与产生的验证码不一致时    
-              console.log(this.picLyanzhengma.toUpperCase())
-              console.log(code)           
-              alert("验证码错误！")
-              // $(".login_content1 span:eq(2)").removeClass("disappear");
-              this.createCode();//刷新验证码   
-              this.picLyanzhengma = '';
-          }else { //输入正确时   
-              // $(".login_content1 span:eq(2)").addClass("disappear");
-              // $(".login_content1 span:eq(2)").text("请输入验证码")
-              return true;
-          } 
+      for(var i = 0; i < codeLength; i++) {//循环操作   
+        var index = Math.floor(Math.random()*36);//取得随机数的索引（0~35）   
+        code += random[index];//根据索引取得随机数加到code上   
+      }   
+      this.checkCode = code;//把code值赋给验证码   
+    },
+    // 失焦验证图和密码
+    checkLpicma(){
+      this.picLyanzhengma.toUpperCase();//取得输入的验证码并转化为大写         
+      if(this.picLyanzhengma == '') {
+        // $(".login_content1 span:eq(2)").text("请输入验证码")
+        // $(".login_content1 span:eq(2)").removeClass("disappear");
+      }else if(this.picLyanzhengma.toUpperCase() != this.checkCode ) { //若输入的验证码与产生的验证码不一致时    
+        console.log(this.picLyanzhengma.toUpperCase())
+        console.log(code)           
+        alert("验证码错误！")
+        // $(".login_content1 span:eq(2)").removeClass("disappear");
+        this.createCode();//刷新验证码   
+        this.picLyanzhengma = '';
+      }else { //输入正确时   
+        // $(".login_content1 span:eq(2)").addClass("disappear");
+        // $(".login_content1 span:eq(2)").text("请输入验证码")
+        return true;
+      } 
 
-      },
-    GLYlogin(){
+    },
+    //登录
+    login(){
       let vm = this;
-      if (this.GLYusername == null || this.GLYusername == '') {
-        alert("用户名不能为空！")
-      } else if (this.GLYpassword == null || this.GLYpassword == '') {
-        alert("密码不能为空！")
-      } 
-      else if(this.picLyanzhengma == null || this.picLyanzhengma == ''){
-        alert("验证码不能为空！")
-      } 
-      else if(this.checkLpicma() == true){
+      //校验
+      if (!/^1[34578]\d{9}$/.test(vm.username)) {
+        vm.usernameAlertFlag = true;
+      }else if (vm.password == '' || vm.password == null) {
+        vm.passwordAlertFlag = true;
+      }else if(vm.picLyanzhengma.toUpperCase() != vm.checkCode){
+        vm.validateAlertFlag = true;
+        //刷新Verification Code 
+        vm.createCode();  
+        vm.picLyanzhengma = '';
+      }
+      else{
         var params = {
-          username: vm.GLYusername,
-          password: vm.GLYpassword,
-          loginType: vm.GLYloginType,
+          username: vm.username,
+          password: vm.password,
+          //loginType: vm.GLYloginType,
           usertype: "CHN",
           deptid: "ZSYH"
         }
@@ -151,85 +173,14 @@ export default {
       }
     }
   },
-    created(){
-        this.createCode();
-    }
+  created(){
+    this.createCode();
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-// @import '../../common/scss/all.scss';
-$height: 22rem; //全局变量声明
-$width: 22rem;
-$heightimg: 8.75rem;
-$widthimg: 8.75rem;
-$heighttitle: 90%;
-$widthtitle: 90%;
-$bottomfooter: 3.125rem;
-$heightfooter: 3.125rem;
-$widthlgbtn: 12rem;
-$line-heightfooter: 3.125rem;
-$widthfooter: 100%;
-$bgcolor: #ea2530;
-$bgcolor2: rgb(234, 109, 37);
-$whitecolor: #fff;
-$color166: rgb(251, 106, 116);
-$top0: 0;
-$left0: 0;
-$right0: 0;
-$bottom0: 0;
-$blackcolor: #2c2c2c;
-.login {
-  position: absolute;
-  top: $top0;
-  left: $top0;
-  right: $right0;
-  bottom: $bottom0;
-  overflow: hidden;
-  overflow-y: auto;
-  z-index: 1000;
-  background: url("/static/images/login/login.png") center 100% #65809d;
-
-  * {
-    font-size: 1rem;
-  }
-
-  p {
-    margin: 4rem 0 0 6rem;
-  }
-
-  .imgstyle {
-    height: $heightimg;
-    width: $widthimg;
-  }
-
-  .titlestyle {
-    height: $heighttitle;
-    width: $widthtitle;
-  }
-
-  .pstyle {
-    margin: 5rem 0 0 0;
-  }
-}
-
-.lana {
-  color: $whitecolor;
-  font-size: 1rem;
-  cursor: pointer;
-  letter-spacing: 0.1rem;
-}
-
-.lanb {
-  color: $blackcolor;
-  font-size: 1rem;
-  cursor: pointer;
-}
-
-.filed {
-  margin: 0px 1.875rem 1rem 1.875rem;
-}
-
+@import '@/common/scss/login.scss';
 .lgin {
   margin-top: 5rem;
   .el-button {
@@ -241,43 +192,6 @@ $blackcolor: #2c2c2c;
     background-color: $bgcolor2;
   }
 }
-
-.right {
-  text-align: right;
-  text-align: -webkit-right;
-  color: $color166;
-}
-
-.left {
-  color: $color166;
-  padding-top: 30px;
-  text-align: -webkit-left;
-}
-
-.login-form {
-  height: 400px;
-  width: 400px;
-  background: url("/static/images/login/form_bg.png") no-repeat;
-  background-size:100% 100%; 
-  .signstyle {
-    margin-left: 6.5rem;
-    a {
-      cursor: pointer;
-    }
-  }
-}
-
-.el-button {
-  border: 0px solid #dcdfe6;
-}
-
-.login-btn {
-  color: $whitecolor;
-  background-color: $bgcolor;
-  font-size: 1rem;
-  margin-top: 0.5rem;
-}
-
 .footer {
   text-align: -webkit-center;
   color: $whitecolor;
@@ -287,272 +201,6 @@ $blackcolor: #2c2c2c;
     color: $whitecolor;
     font-size: 1rem;
   }
-}
-
-.footerstyle {
-  position: absolute;
-  bottom: $bottomfooter;
-  height: $heightfooter;
-  text-align: center;
-  line-height: $line-heightfooter;
-  width: $widthfooter;
-}
-
-.logincenter {
-  margin-top: 3rem;
-}
-
-.muchtab {
-  a {
-    cursor: pointer;
-  }
-}
-
-.clearfix:after{
-    content:'';
-    display:block;
-    height:0;
-    clear:both;
-}
-.login_or_register{
-    background-color: #f2f2f5;
-    padding-bottom: 50px;
-}
-/*logo*/
-.logo1{
-    text-align: center;
-    padding:40px 0 50px ;
-    margin:0 auto;
-}
-/*login和register部分*/
-.login_bac{
-    width:1180px;
-    height: 728px;
-    background-size: 1180px 728px;
-    margin: 0 auto;
-    position: relative;
-}
-.register_content{
-    width:400px;
-    background-color: #fff;
-    box-shadow: 0 0 15px rgba(0, 0, 0, .5);
-    position: absolute;
-    left:60px;
-    top:80px;
-    padding-left: 30px;
-    padding-bottom: 50px;
-}
-.tianxie_img{
-    width:26px;
-    vertical-align: middle;
-    margin-right: 10px;
-}
-.tishixiaoxi{
-    font-size: 14px;
-    color:#ed711f;
-    display: block;
-    margin-left: 30px;
-    line-height: 30px;
-}
-.yanzhengma_input{
-  width: 220px;
-}
-.disappear{
-     visibility:hidden;
-}
-.register_content p{
-    margin-top: 30px;
-    font-size: 25px;
-    line-height: 18px;
-    margin-bottom: 15px;
-}
-.register_content input{
-    padding: 5px 0 5px 10px;
-    margin-left: 30px;
-    height: 30px;
-    border: 1px solid #e6e6e6;
-}
-.register_content_input{
-    width:300px;
-    height: 30px;
-    padding: 5px 0 5px 10px;
-    border: 1px solid #e6e6e6;
-    margin-left: 30px;
-}
-.verification{
-    vertical-align: middle;
-    margin-left: 10px;
-}
-.agreement{
-    position: relative;
-    top: -10px;
-}
-.read_already{
-    position: relative;
-    top:-21px;
-    font-size: 12px;
-    color: #999;
-    cursor: pointer;
-    margin-left: 5px;
-}
-.read_already a{
-    color:#053d84;
-}
-.send_message{
-    display: inline-block;
-    height: 40px;
-    line-height: 40px;
-    color:#ccc;
-    padding-left: 45px;
-    padding-right: 5px;
-    background: url('/static/img/message.png') no-repeat 10px 8px;
-    background-size: 25px;
-    border: 1px solid #f3f3f3;
-    margin-left: 10px;
-    cursor: pointer;
-}
-.next{
-    display: block;
-    width: 287px;
-    height: 50px;
-    font-size: 18px;
-    text-align: center;
-    line-height: 50px;
-    color: #fff;
-    background-color: #053d84;
-    border-radius: 5px;
-    margin-left: 40px;
-    cursor: pointer;
-
-}
-/*登录模块*/
-.login_content1{
-    width:400px;
-    background-color: #fff;
-    box-shadow: 0 0 15px rgba(0, 0, 0, .5);
-    position: absolute;
-    right:60px;
-    top:80px;
-    padding-left: 30px;
-    padding-bottom: 50px;
-}
-.user_login{
-    display: block;
-    width: 287px;
-    height: 50px;
-    font-size: 18px;
-    text-align: center;
-    line-height: 50px;
-    color: #fff;
-    background-color: #a2be3c;
-    border-radius: 5px;
-    margin-left: 40px;
-    cursor:pointer;
-}
-.login_content1 p{
-    margin-top: 30px;
-    font-size: 25px;
-    line-height: 18px;
-    margin-bottom: 15px;
-}
-.login_content1 input{
-    padding: 5px 0 5px 10px;
-    margin-left: 30px;
-    height: 30px;
-    border: 1px solid #e6e6e6;
-}
-.verification1{
-    // vertical-align: middle;
-    // transform: translate(-15px,0);
-    height: 2.5rem;
-    width: 7.1875rem;
-}
-.forget{
-    text-align: right;
-    position: relative;
-    top:-20px;
-    cursor: pointer;
-}
-.forget a{
-    font-size: 14px;
-    color:#053d84;
-    margin-right: 80px;
-}
-#code{
-    font-size: 18px;
-    letter-spacing:3px;
-    color: #053d84;
-    background: #f2f2f5;
-}
-/*弹窗*/
-.tanchuang{
-    position: fixed;
-    top:0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color:rgba(0,0,0,.5);
-    z-index:100;
-}
-.hiddenTanchuang{
-    display: none;
-}
-.inTanchuang{
-    position: absolute;
-    text-align: center;
-    width: 300px;
-    height: 169px;
-    padding-top: 10px;
-    left: 50%;
-    top: 50%;
-    border-radius: 10px;
-    -webkit-transform: translateX(-50%) translateY(-50%);
-    -ms-transform: translateX(-50%) translateY(-50%);
-    transform: translateX(-50%) translateY(-50%);
-    background-color: #fff;
-}
-.errorIcon{
-    width:55px;
-}
-.register_content  .cue{
-    font-size: 14px;
-    text-align: center;
-    margin-top: 15px;
-}
-.sure{
-    display: inline-block;
-    color: #fff;
-    width: 100px;
-    height: 20px;
-    padding: 6px 10px;
-    background: #053D84;
-    border-radius: 5px;
-    font-size: 18px;
-    line-height: 18px;
-    margin-top: 10px;
-    cursor: pointer;
-}
-
-a {
-    text-decoration: none;
-    color:$bgcolor;
-}
-
-.icou{
-  font-size:2.2rem;
-}
-
-.bk{
-  color:yellow !important;
-  background:chocolate !important;
-  border:0px;
-}
-
-.alert{
-  position:absolute;
-  top:68px;
-  margin-left:0px !important;
-  color:#EA2530;
 }
 
 </style>
