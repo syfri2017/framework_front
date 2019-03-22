fafdba7140d59cc0016770a9d89184cd05bc5706
lixue0919 <442019696@qq.com>
2019-03-22 11:17:39 +0800

    英文版忘记用户名代码翻新  by li.xue 2019/3/22

--- src/views/exhibition/login/en/ForgetUsername.vue
+++ src/views/exhibition/login/en/ForgetUsername.vue
@@ -9,13 +9,15 @@
             <span class="formTitleStyle">Username Recovery</span>
             <span class="signstyle"><router-link :to="{path:'/exhibition/login/en/login'}"><a @click="open()">Back to Login</a></router-link></span>
           </div>
-          <form ref="GLYloginForm" id="GLYloginForm" autocomplete="off" name="loginform"  method="post">
+          <form ref="loginForm" id="loginForm" autocomplete="off" name="loginform"  method="post">
             <div class="filed">
-              <el-input placeholder="Association Email" v-model="FUmail" name="FUmail" id="FUmail" prefix-icon="iconfont icon-youxiang"></el-input>
-              <button type="button" id="FUmail-btn" class="verficode phonebtnEN" @click="getFUMailCode()" v-text=FUmailCodeText :disabled="FUmailBtnDisabled"></button>
+              <el-input placeholder="Association Email" v-model="FUmail" name="FUmail" id="FUmail" prefix-icon="iconfont icon-youxiang1" @blur="FUmailCheck"></el-input>
+              <button type="button" id="FUmail-btn" class="verficode phonebtnEN" @click="getFUMailCode()" v-text=FUmailCodeText></button>
+              <p class="alert" v-show="mailAlertFlag">*Please fill in the correct Email.</p>
             </div>
             <div class="filed">
-              <el-input placeholder="Mail Verification Code"  v-model="FUmailCode" name="FUmailCode" id="FUmailCode" prefix-icon="iconfont icon-youxiang1"></el-input>
+              <el-input placeholder="Mail Verification Code"  v-model="FUmailCode" name="FUmailCode" id="FUmailCode" prefix-icon="iconfont icon-validate" @blur="mailCodeCheck"></el-input>
+              <p class="alert1" v-show="mailCodeAlertFlag">*The verification code is incorrect.</p>
             </div>
             <div class="filed lgin">
               <el-button type="danger" @click="FUIdentify()" round>Confirm</el-button>
@@ -32,20 +34,22 @@ export default {
   name: 'Login',
   data () {
     return {
-         //忘记用户名
-        FUmail: "",
-        FUmailCode: "",
-        FUmailCodeReal: "",
-        FUmailCodeText: "Get Verification Code",
-        FUtimer: null,
-        FUmailBtnDisabled: false,
-        //忘记用户名
-        time: 60
+      //忘记用户名
+      FUmail: "",
+      FUmailCode: "",
+      FUmailCodeReal: "",
+      FUmailCodeText: "Get Verification Code",
+      FUtimer: null,
+      time: 60,
+      //验证邮箱标识位
+      mailAlertFlag: false,
+      mailCodeAlertFlag: false
     }
   },
   methods:{
-     //消息提示框
-      open() {
+    //消息提示框
+    open() {
+      if (this.FUmail!='' || this.FUmailCode!='') {
         this.$alert('Unsaved data will be lost. Are you sure to return it?', 'Tips', {
           confirmButtonText: 'Sure',
           callback: action => {
@@ -55,86 +59,92 @@ export default {
             });
           }
         });
-      },
-    //忘记用户名
-        FUmailCheck() {
-            if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FUmail))) {
-                // 邮箱格式不正确
-                alert("The email format is not correct");
-                return false;
-            } else {
-                return true;
-            }
-        },
-        getFUMailCode() {
-            let vm = this;
-            this.FUmailCode = "";
-            if (this.FUmailCheck()) {
-                this.FUmailCodeText = "Sending...";
-                this.FUmailBtnDisabled = true;
-                vm.$axios.get('/signin/getMailNumENG/' + this.FUmail + "/static").then(function (res) {
-                    if (res.data.result == 0) { 
-                        // 该邮箱未注册！
-                        alert("The email is not registered!");
-                        this.FUmailCodeText = "Get Verification Code";
-                        this.FUmailBtnDisabled = false;
-                    } else if (res.data.result == 1) {
-                        vm.$axios.get('/signin/sendMailEng?mail=' + this.FUmail).then(function (res) {
-                           
-                            this.FUmailCodeReal = res.data.msg;
-                            var count = this.time;
-                            this.timer = setInterval(() => {
-                                if (count == 0) {
-                                    clearInterval(this.timer);
-                                    this.timer = null;
-                                    this.FUmailCodeText = "Get Verification Code";
-                                    this.FUmailBtnDisabled = false;
-                                } else {
-                                    this.FUmailCodeText = count + "seconds later"
-                                    count--;
-                                    this.FUmailBtnDisabled = true;
-                                }
-                            }, 1000)
-                        }.bind(this), function (error) {
-                            console.log(error);
-                        });
-                    }
-                }.bind(this), function (error) {
-                    console.log(error);
-                });
-            }
-        },
-        FUIdentify() {
-            let vm = this;
-            if (this.FUmail == null || this.FUmail == '') {
-                alert("The email can not be empty!")
-            } else if (this.FUmailCode == null || this.FUmailCode == '') {
-                alert("The verification code can not be empty!")
-            } else {
-                if (this.FUmailCode == this.FUmailCodeReal) {
-                    vm.$axios.get('/signin/getMailNumENG/' + this.FUmail + "/static").then(function (res) {
-                        if (res.data.result == 0) {
-                            alert("The email is not registered!");
-                        } else if (res.data.result == 1) {
-                            vm.$axios.get('/signin/getUsernameByMail/' + this.FUmail + "/static").then(function (res) {
-                                alert("User name back to success!");
-                                this.username = res.data;
-                                // this.changeForm('loginFlag');
-                            }.bind(this), function (error) {
-                                console.log(error);
-                            });
-                        }
-                    }.bind(this), function (error) {
-                        console.log(error);
-                    });
+      }
+    },
 
+    //邮箱校验
+    FUmailCheck() {
+      if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FUmail))) {
+        this.mailAlertFlag = true;
+        return false;
+      } else {
+        this.mailAlertFlag = false;
+        return true;
+      }
+    },
+
+    //邮箱验证码验证
+    mailCodeCheck() {
+      if (this.FUmailCode != this.FUmailCodeReal || this.FUmailCode == '') {
+        this.mailCodeAlertFlag = ture;
+        return false;
+      } else {
+        this.mailCodeAlertFlag = false;
+        return true;
+      }
+    },
+
+    //获取验证码
+    getFUMailCode() {
+      let vm = this;
+      this.FUmailCode = "";
+      if (this.FUmailCheck()) {
+        this.FUmailCodeText = "Sending...";
+        this.FUmailBtnDisabled = true;
+        vm.$axios.get('/signin/getMailNumENG/' + this.FUmail + "/static").then(function (res) {
+          if (res.data.result == 0) { 
+              // 该邮箱未注册！
+            alert("The email is not registered!");
+            this.FUmailCodeText = "Get Verification Code";
+            this.FUmailBtnDisabled = false;
+          } else if (res.data.result == 1) {
+            vm.$axios.get('/signin/sendMailEng?mail=' + this.FUmail).then(function (res) {                 
+              this.FUmailCodeReal = res.data.msg;
+              var count = this.time;
+              this.timer = setInterval(() => {
+                if (count == 0) {
+                  clearInterval(this.timer);
+                  this.timer = null;
+                  this.FUmailCodeText = "Get Verification Code";
+                  this.FUmailBtnDisabled = false;
                 } else {
-                    //验证码输入错误，请核对后再试 
-                    alert("Verification code input error, please check and try again");
+                  this.FUmailCodeText = count + "seconds later"
+                  count--;
+                  this.FUmailBtnDisabled = true;
                 }
-            }
-        },
-
+              }, 1000)
+            }.bind(this), function (error) {
+              console.log(error);
+            });
+          }
+        }.bind(this), function (error) {
+          console.log(error);
+        });
+      }
+    },
+    FUIdentify() {
+      let vm = this;
+      if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FUmail))) {
+        this.mailAlertFlag = true;
+      } else if (this.FUmailCode != this.FUmailCodeReal || this.FUmailCode == '') {
+        this.mailCodeAlertFlag = true;
+      } else {
+        vm.$axios.get('/signin/getMailNumENG/' + this.FUmail + "/static").then(function (res) {
+          if (res.data.result == 0) {
+            alert("The email is not registered!");
+          } else if (res.data.result == 1) {
+            vm.$axios.get('/signin/getUsernameByMail/' + this.FUmail + "/static").then(function (res) {
+              alert("User name back to success!");
+              vm.$router.push({name:"exhibition/login/en/Login", query: {username: res.data, type: 'forgetUsername'}});
+            }.bind(this), function (error) {
+              console.log(error);
+            });
+          }
+        }.bind(this), function (error) {
+          console.log(error);
+        });
+      }
+    }
   }
 }
 </script>
