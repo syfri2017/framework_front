9c4d45c65181dd9e4b09bb1161fe535f0ce063f0
lixue0919 <442019696@qq.com>
2019-03-22 13:51:57 +0800

    lixue0919

--- src/views/exhibition/login/en/ForgetPassword.vue
+++ src/views/exhibition/login/en/ForgetPassword.vue
@@ -1,30 +1,32 @@
 <template>
-    <!-- 用户登录 -->
-    <el-row class="logincenter">
-      <el-col :span="8" >&nbsp;</el-col>
-      <el-col :span="8" style="text-align:-webkit-center">
-        <div class="login-form" >
-          <div class="filed left">
-            <i class="iconfont icou"></i>
-            <span class="formTitleStyle">Password Modification</span>
-            <span class="signstyle"><router-link :to="{path:'/exhibition/login/en/login'}"><a @click="open()">Return Login</a></router-link></span>
-          </div>
-          <form ref="GLYloginForm" id="GLYloginForm" autocomplete="off" name="loginform"  method="post">
-            <div class="filed">
-            <el-input placeholder="Email" v-model="FPBmail" name="FPBmail" id="FPBmail" prefix-icon="iconfont icon-youxiang"></el-input>
-            <button type="button" id="FUmail-btn" class="verficode phonebtnEN" @click="getFPBMailCode11()" v-text=FPBmailCodeText :disabled="FPBmailBtnDisabled"></button>
-            </div>
-            <div class="filed">
-            <el-input placeholder="Mail Verification Code"  v-model="FPBmailCode" name="FPBmailCode" id="FPBmailCode" prefix-icon="iconfont icon-youxiang1"></el-input>
-            </div>
-            <div class="filed lgin">
-            <el-button type="danger" @click="FPBIdentifyen()" round>Confirm</el-button>
-            </div>
-          </form>
+  <!-- 用户登录 -->
+  <el-row class="logincenter">
+    <el-col :span="8" >&nbsp;</el-col>
+    <el-col :span="8" style="text-align:-webkit-center">
+      <div class="login-form" >
+        <div class="filed left">
+          <i class="iconfont icou"></i>
+          <span class="formTitleStyle">Password Modification</span>
+          <span class="signstyle"><router-link :to="{path:'/exhibition/login/en/login'}"><a @click="open()">Return Login</a></router-link></span>
         </div>
-      </el-col>
-      <el-col :span="8">&nbsp;</el-col>
-    </el-row>
+        <form ref="loginForm" id="loginForm" autocomplete="off" name="loginform"  method="post">
+          <div class="filed">
+            <el-input placeholder="Email" v-model="FPBmail" name="FPBmail" id="FPBmail" prefix-icon="iconfont icon-youxiang1" @blur="FPBmailCheck"></el-input>
+            <button type="button" id="FUmail-btn" class="verficode phonebtnEN" @click="getFPBMailCode()" v-text=FPBmailCodeText :disabled="FPBmailBtnDisabled"></button>
+            <p class="alert" v-show="mailAlertFlag">*The email is incorrect.</p>
+          </div>
+          <div class="filed">
+            <el-input placeholder="Mail Verification Code"  v-model="FPBmailCode" name="FPBmailCode" id="FPBmailCode" prefix-icon="iconfont icon-validate" @blur="mailCodeCheck"></el-input>
+            <p class="alert1" v-show="mailCodeAlertFlag">*The verification code is incorrect.</p>
+          </div>
+          <div class="filed lgin">
+            <el-button type="danger" @click="FPBIdentify()" round>Confirm</el-button>
+          </div>
+        </form>
+      </div>
+    </el-col>
+    <el-col :span="8">&nbsp;</el-col>
+  </el-row>
 </template>
 
 <script>
@@ -32,24 +34,24 @@ export default {
   name: 'Login',
   data () {
     return {
-       //忘记密码
-        FPBmail: "",
-        FPBmailCode: "",
-        FPBmailCodeReal: "",
-        FPBmailCodeText: "Get Verification Code",
-        FPBtimer: null,
-        FPDusername: "",
-        FPDpassword1: "",
-        FPDpassword2: "",
-        FPDregisterData: "",
-        FPBmailBtnDisabled: false,
-        //忘记密码
-        time: 60
+      //忘记密码
+      FPBmail: "",
+      FPBmailCode: "",
+      FPBmailCodeReal: "",
+      FPBmailCodeText: "Get Verification Code",
+      FPBtimer: null,
+      time: 60,
+      //验证邮箱标识位
+      mailAlertFlag: false,
+      mailCodeAlertFlag: false,
+      //获取邮箱验证码是否可点击
+      FPBmailBtnDisabled: false
     }
   },
   methods:{
-       //消息提示框
-     open() {
+    //消息提示框
+    open() {
+      if (this.FPBmail!='' || this.FPBmailCode!='') {
         this.$alert('Unsaved data will be lost. Are you sure to return it?', 'Tips', {
           confirmButtonText: 'Sure',
           callback: action => {
@@ -59,157 +61,86 @@ export default {
             });
           }
         });
-      },
-   //忘记密码
-        //B
-        FPBmailCheck() {
-            if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FPBmail))) {
-                //邮箱格式不正确
-                alert("The email format is not correct");
-                return false;
-            } else {
-                return true;
-            }
-        },
-        getFPBMailCode11() {
-            let vm = this;
-            this.FPBmailCode = "";
-            if (this.FPBmailCheck()) {
-                this.FPBmailCodeText = "Sending...";
-                this.FPBmailBtnDisabled = true;
-                vm.$axios.get('/signin/getUsernameNum/' + this.FPBmail + "/static").then(function (res) {
-                    if (res.data.result == 0) {
-                        //该邮箱未注册！
-                        alert("The email is not registered!");
-                        this.FPBmailCodeText = "Get Verification Code";
-                        this.FPBmailBtnDisabled = false;
-                    } else if (res.data.result == 1) {
-                        vm.$axios.get('/signin/sendMailEng?mail=' + this.FPBmail).then(function (res) {
-                            this.FPBmailCodeReal = res.data.msg;
-                            var count = this.time;
-                            this.FPBtimer = setInterval(() => {
-                                if (count == 0) {
-                                    clearInterval(this.FPBtimer);
-                                    this.FPBtimer = null;
-                                    this.FPBmailCodeText = "Get Verification Code";
-                                    this.FPBmailBtnDisabled = false;
-                                } else {
-                                    this.FPBmailCodeText = count + "seconds later"
-                                    count--;
-                                    this.FPBmailBtnDisabled = true;
-                                }
-                            }, 1000)
-                        }.bind(this), function (error) {
-                            console.log(error);
-                        });
-                    }
-                }.bind(this), function (error) {
-                    console.log(error);
-                });
-            }
-        },
-        changeForm(flag) {
-            if (flag == 'loginFlag') {
-            } else if (flag == 'regFlag') {
-            } else if (flag == 'FUAFlag') {
-                this.FUmail = "";
-                this.FUmailCode = "";
-            } else if (flag == 'FPBFlag') {
-                this.FPBmail = "";
-                this.FPBmailCode = "";
-                this.FPBmailCodeReal = "";
-                this.FPBmailCodeText = "Get Verification Code";
-                this.FPBtimer = null;
-            } else if (flag == 'FPDFlag') {
-                this.FPDusername = "";
-                this.FPDpassword1 = "";
-                this.FPDpassword2 = "";
-            } else if (flag == 'bakFlag') {
-                // 未保存的数据将丢失，确定返回吗？
-                var r = confirm("Unsaved data will be lost. Do you want to return?");
-                if (r == false) {
-                    return;
-                }
-            }
-            this.formFlag = flag;
-        },
-        FPBIdentifyen() {
-          
-            let vm = this;
-            if (this.FPBmail == null || this.FPBmail == '') {
-                // 邮箱不能为空！
-                alert("The email can not be empty!")
-            } else if (this.FPBmailCode == null || this.FPBmailCode == '') {
-                //验证码不能为空!
-                alert("The verification code can not be empty!")
-            } else {
-                if (this.FPBmailCode == this.FPBmailCodeReal) {
-                      //  console.log(2222222222222)
-                      vm.$axios.get('/signin/findByUsername/' + this.FPBmail + "/static").then(function (res) {
-                      // console.log(11111111)
-                       alert("Verification code input error, please check and try again.");
-                      this.changeForm('FPDFlag');
-                      this.FPDregisterData = res.data.result;
-                      this.FPDusername = this.FPDregisterData[0].username;
-                      // alert("请输入新密码！");
-                    }.bind(this), function (error) {
-                        console.log(error);
-                    });
-                } else {
-                    //验证码输入错误，请核对后再试
-                    alert("Verification code input error, please check and try again.");
-                }
-            }
-        },
-        //D
-        FPDpassword1Check() {
-            if (!(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(this.FPDpassword1))) {
-                this.FPDpassword1TipFlag = false;
-                this.FPDpassword1AlertFlag = true;
-                return false;
-            } else {
-                this.FPDpassword1TipFlag = false;
-                this.FPDpassword1AlertFlag = false;
-                return true;
-            }
-        },
-        FPDpassword1Tip() {
-            this.FPDpassword1TipFlag = true;
-            this.FPDpassword1AlertFlag = false;
-        },
-        FPDpassword2Check() {
-            if (this.FPDpassword1 !== this.FPDpassword2) {
-                this.FPDpassword2AlertFlag = true;
-                return false;
-            } else {
-                this.FPDpassword2AlertFlag = false;
-                return true;
-            }
-        },
-        FPregister() {
-            let vm = this;
-            this.FPDpassword1Check();
-            this.FPDpassword2Check();
-            if (this.FPDpassword1Check() && this.FPDpassword2Check() && this.FPDmessageCode == this.FPDmessageCodeReal) {
-                var params = {
-                    userid: this.FPDregisterData[0].userid,
-                    username: this.FPDusername,
-                    password: this.FPDpassword1,
-                }
-                vm.$axios.post('/xfxhapi/signin/updateByVO', params).then(function (res) {
-                    if (res.data.result == 1) {
-                        //密码修改成功！
-                        alert("Password changed successfully!");
-                        this.username = this.FPDusername;
-                        this.password = this.FPDpassword1;
-                        // this.changeForm('loginFlag');
-                    }
-                }.bind(this), function (error) {
-                    console.log(error)
-                })
-            }
-        },
+      }
+    },
 
+    //邮箱格式验证
+    FPBmailCheck() {
+        if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FPBmail))) {
+          //邮箱格式不正确
+          this.mailAlertFlag = true;
+          return false;
+        } else {
+          this.mailAlertFlag = false;
+          return true;
+        }
+    },
+
+    //邮箱验证码验证
+    mailCodeCheck() {
+      if (this.FPBmailCode != this.FPBmailCodeReal || this.FPBmailCode == '') {
+        this.mailCodeAlertFlag = true;
+        return false;
+      } else {
+        this.mailCodeAlertFlag = false;
+        return true;
+      }
+    },
+
+    //获取邮箱验证码
+    getFPBMailCode() {
+      let vm = this;
+      this.FPBmailCode = "";
+      if (this.FPBmailCheck()) {
+        this.FPBmailCodeText = "Sending...";
+        this.FPBmailBtnDisabled = true;
+        vm.$axios.get('/signin/getUsernameNum/' + this.FPBmail + "/static").then(function (res) {
+          if (res.data.result == 0) {
+            //该邮箱未注册！
+            alert("The email is not registered!");
+            this.FPBmailCodeText = "Get Verification Code";
+            this.FPBmailBtnDisabled = false;
+          } else if (res.data.result == 1) {
+            vm.$axios.get('/signin/sendMailEng?mail=' + this.FPBmail).then(function (res) {
+              this.FPBmailCodeReal = res.data.msg;
+              var count = this.time;
+              this.FPBtimer = setInterval(() => {
+                if (count == 0) {
+                  clearInterval(this.FPBtimer);
+                  this.FPBtimer = null;
+                  this.FPBmailCodeText = "Get Verification Code";
+                  this.FPBmailBtnDisabled = false;
+                } else {
+                  this.FPBmailCodeText = count + "seconds later"
+                  count--;
+                  this.FPBmailBtnDisabled = true;
+                }
+              }, 1000)
+            }.bind(this), function (error) {
+                console.log(error);
+            });
+          }
+        }.bind(this), function (error) {
+            console.log(error);
+        });
+      }
+    },
+      
+    //Confirm点击事件
+    FPBIdentify() {
+      let vm = this;
+      if (!(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)+$/.test(this.FPBmail))) {
+        this.mailAlertFlag = true;
+      } else if (this.FPBmailCode != this.FPBmailCodeReal || this.FPBmailCode == '') {
+        this.mailCodeAlertFlag = true;
+      } else {
+        vm.$axios.get('/signin/findByUsername/' + this.FPBmail + "/static").then(function (res) {
+          vm.$router.push({name:"exhibition/login/en/Mailbox", query: {username: vm.FPBmail, userid: res.data.result[0].userid}});
+        }.bind(this), function (error) {
+            console.log(error);
+        }); 
+      }
+    }
   }
 }
 </script>
---
+++ src/views/exhibition/login/en/Mailbox.vue
@@ -0,0 +1,127 @@
+<template>
+    <el-row class="logincenter">
+      <el-col :span="8" >&nbsp;</el-col>
+      <el-col :span="8" style="text-align:-webkit-center">
+        <div class="login-form" >
+          <div class="filed left">
+            <i class="iconfont icou"></i>
+            <span class="formTitleStyle">Password Modification</span>
+            <span class="signstyle"><router-link :to="{path:'/exhibition/login/en/login'}"><a @click="open()">To Login</a></router-link></span>
+          </div>
+          <form ref="loginForm" id="loginForm" autocomplete="off" name="loginform"  method="post">
+            <div class="filed">
+              <el-input placeholder="Please input an email" v-model="FPDusername" name="FPDusername" id="FPDusername" prefix-icon="iconfont icon-youxiang1" :disabled="FPDusernameFlag"></el-input>
+            </div>
+            <div class="filed">
+              <el-input placeholder="Please input a password" prefix-icon="iconfont icon-password" type="password" class="inputstyle" v-model="FPDpassword1" name="FPDpassword1"
+                id="FPDpassword1" @blur="FPDpassword1Check"></el-input>
+              <p class="alert1" v-show="FPDpassword1AlertFlag">*Password must be 6-16-bit alphanumeric combination.</p>
+            </div>
+            <div class="filed">
+              <el-input type="password" class="inputstyle" v-model="FPDpassword2" name="FPDpassword2" id="FPDpassword2"
+                placeholder="Please input the password again" @blur="FPDpassword2Check" prefix-icon="iconfont icon-querenmima-copy"></el-input>
+              <p class="alert2" v-show="FPDpassword2AlertFlag">*The two entries do not match.</p>
+            </div>
+            <div class="filed lgin">
+            <el-button type="danger" @click="updatePassword()" round>Confirm</el-button>
+            </div>
+          </form>
+        </div>
+      </el-col>
+      <el-col :span="8">&nbsp;</el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      FPDuserid: "",
+      FPDusername: "",
+      FPDpassword1: "",
+      FPDpassword2: "",
+      FPDregisterData: "",
+      //密码和确认密码标识符
+      FPDpassword1AlertFlag: false,
+      FPDpassword2AlertFlag: false,
+      //用户名文本框Disabled
+      FPDusernameFlag: true
+    }
+  },
+  created: function() {
+    this.FPDuserid = this.$route.query.userid;
+    this.FPDusername = this.$route.query.username;
+  },
+  methods:{
+    //消息提示框
+    open() {
+      if (this.FPDpassword1!='' || this.FPDpassword2!='') {
+        this.$alert('Unsaved data will be lost. Are you sure to return it?', 'Tips', {
+          confirmButtonText: 'Sure',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `Unsaved data will be lost!`
+            });
+          }
+        });
+      }
+    },
+
+    //密码规则校验
+    FPDpassword1Check() {
+      if (!(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(this.FPDpassword1))) {
+        this.FPDpassword1AlertFlag = true;
+        return false;
+      } else {
+        this.FPDpassword1AlertFlag = false;
+        return true;
+      }
+    },
+
+    //确认密码校验
+    FPDpassword2Check() {
+      if (this.FPDpassword1 !== this.FPDpassword2) {
+        this.FPDpassword2AlertFlag = true;
+        return false;
+      } else {
+        this.FPDpassword2AlertFlag = false;
+        return true;
+      }
+    },
+
+    //修改密码
+    updatePassword() {
+      let vm = this;
+      if (!(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(this.FPDpassword1))) {
+        vm.FPDpassword1AlertFlag = true;
+      } else if (this.FPDpassword1 !== this.FPDpassword2) {
+        vm.FPDpassword2AlertFlag = true 
+      } else {
+        var params = {
+          userid: this.FPDuserid,
+          username: this.FPDusername,
+          password: this.FPDpassword1,
+        }
+        vm.$axios.post('/signin/updateByVO', params).then(function (res) {
+          console.log(res);
+          if (res.data.result == 1) {
+            alert("Password changed successfully!");
+            vm.$router.push({name:"exhibition/login/en/Login", query: {username: this.FPDusername, type: 'forgetPassword'}});
+          }
+        }.bind(this), function (error) {
+            console.log(error)
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/common/scss/login.scss';
+.lgin {
+  margin-top: 7.25rem;
+}
+</style>
