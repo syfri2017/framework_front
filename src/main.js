cec706cc46d1e9632e221e644f9a67d8d6792ce7
lixue0919 <442019696@qq.com>
2019-03-22 13:31:59 +0800

    未登陆修改密码，取消拦截  by li.xue 2019/3/22

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ axios.interceptors.request.use(
         && config.url.indexOf('/signin/getUsernameByMail')>-1 && config.url.indexOf('/signin/findByMail')>-1 
         && config.url.indexOf('/signin/findByUsername')>-1 && config.url.indexOf('/signin/insertByVO')>-1
         && config.url.indexOf('/signin/findByUnscid/')>-1 && config.url.indexOf('/signin/getMailNum/')>-1
-        && config.url.indexOf('/signin/sendMail')>-1
+        && config.url.indexOf('/signin/sendMail')>-1 && config.url.indexOf('/signin/updateByVO')>-1
         ){
       var isLogin = localStorage.getItem('isLogin');
       if(isLogin != 'TRUE'){
